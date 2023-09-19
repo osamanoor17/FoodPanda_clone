bf561bd1ee8099473f9a5601bb7ce44defd42f48
Muhammad Osama Noor <100303780+Meizzosama@users.noreply.github.com>
2023-09-19 15:11:41 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	lib/help.dart
    #	lib/main.dart
    #	lib/splash_screen/contentpage.dart
    #	lib/splash_screen/sidebar.dart

--- lib/splash_screen/splashscreen.dart
+++ lib/splash_screen/splashscreen.dart
@@ -1,3 +1,193 @@
+// import 'package:flutter/material.dart';
+//
+// import 'contentpage.dart';
+//
+// class SplashScreen extends StatefulWidget {
+//   @override
+//   _SplashScreenState createState() => _SplashScreenState();
+// }
+//
+// class _SplashScreenState extends State<SplashScreen> {
+//   List<Color> MYcolors = [Colors.red, Colors.purple, Colors.orange];
+//
+//   int currentIndex = 0;
+//   late PageController _controller;
+//
+//   @override
+//   void initState() {
+//     _controller = PageController(initialPage: 0);
+//     super.initState();
+//   }
+//
+//   void onPageChanged(int index) {
+//     setState(() {
+//       currentIndex = index;
+//     });
+//   }
+//
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Stack(
+//         children: [
+//           Column(
+//             children: [
+//               Expanded(
+//                 child: PageView.builder(
+//                   controller: _controller,
+//                   itemCount: contents.length,
+//                   onPageChanged: onPageChanged,
+//                   itemBuilder: (_, i) {
+//                     return Padding(
+//                       padding: const EdgeInsets.all(40),
+//                       child: Column(
+//                         children: [
+//                           if (i == 2)
+//                             const Expanded(
+//                               child: Align(
+//                                 alignment: Alignment.topRight,
+//                                 child: SizedBox(),
+//                               ),
+//                             ),
+//                           Padding(
+//                             padding: const EdgeInsets.all(15.0),
+//                             child: Image.asset(
+//                               contents[i].image,
+//                               scale: 0.02,
+//                               height: 300,
+//                             ),
+//                           ),
+//                           const SizedBox(
+//                             height: 60,
+//                           ),
+//                           Row(
+//                             children: [
+//                               Text(
+//                                 contents[i].title1, // Use title data here
+//                                 style: const TextStyle(
+//                                   fontSize: 16,
+//                                   fontWeight: FontWeight.bold,
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                           Row(
+//                             children: [
+//                               Text(
+//                                 contents[i].title2, // Use title data here
+//                                 style: const TextStyle(
+//                                   fontSize: 35,
+//                                   fontWeight: FontWeight.bold,
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                           const SizedBox(height: 20),
+//                           Row(
+//                             children: [
+//                               Text(
+//                                 contents[i]
+//                                     .description, // Use description data here
+//                                 style: const TextStyle(
+//                                   fontSize: 16,
+//                                   color: Colors.black54,
+//                                 ),
+//                               ),
+//                               SizedBox(
+//                                 height: 5,
+//                               ),
+//                             ],
+//                           ),
+//                           if (i == 2) const SizedBox(height: 5),
+//                           if (i == 2)
+//                             ElevatedButton(
+//                               onPressed: () {},
+//                               child: Text(
+//                                 'Get Started',
+//                                 style: TextStyle(
+//                                   fontSize: 12,
+//                                 ),
+//                               ),
+//                             ),
+//                         ],
+//                       ),
+//                     );
+//                   },
+//                 ),
+//               ),
+//               const SizedBox(
+//                 height: 20,
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.all(12.0),
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.start,
+//                   children: List.generate(
+//                     contents.length,
+//                     (index) => buildDot(index, context),
+//                   ),
+//                 ),
+//               ),
+//               Container(
+//                 height: 3,
+//                 margin: const EdgeInsets.all(40),
+//                 width: double.infinity,
+//                 color: MYcolors[currentIndex],
+//               ),
+//             ],
+//           ),
+//           if (currentIndex == 0)
+//             Positioned(
+//               top: 0,
+//               right: 0,
+//               child: GestureDetector(
+//                 onTap: () {
+//                   // Handle the "Skip" button tap
+//                 },
+//                 child: Container(
+//                   padding: const EdgeInsets.all(35.0),
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(8.0),
+//                     child: TextButton(
+//                       child: const Text(
+//                         'Skip',
+//                         style: TextStyle(
+//                           color: Colors.black54,
+//                           fontWeight: FontWeight.bold,
+//                           fontSize: 20,
+//                         ),
+//                       ),
+//                       onPressed: () {},
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   Container buildDot(int index, BuildContext context) {
+//     return Container(
+//       height: 10,
+//       width: currentIndex == index ? 25 : 10,
+//       margin: const EdgeInsets.only(right: 5),
+//       decoration: BoxDecoration(
+//         borderRadius: BorderRadius.circular(20),
+//         color: MYcolors[index],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 import 'contentpage.dart';
@@ -33,7 +223,6 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    bool isFirstPage = currentIndex == 0;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(
@@ -50,28 +239,18 @@ class _SplashScreenState extends State<SplashScreen> {
                       padding: const EdgeInsets.all(40),
                       child: Column(
                         children: [
-                          Expanded(
-                            child: Align(
-                              alignment: Alignment.topRight,
-                              child: SizedBox(
-                                child: Image.asset(
-                                  contents[currentIndex].sidebarImages,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
+                          const SizedBox(
+                            height: 60,
                           ),
                           Padding(
-                            padding: const EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.all(15.0),
                             child: Image.asset(
                               contents[i].image,
                               scale: 0.02,
                               height: 300,
                             ),
                           ),
-                          const SizedBox(
-                            height: 60,
-                          ),
+                          const SizedBox(height: 20),
                           Row(
                             children: [
                               Text(
@@ -106,16 +285,29 @@ class _SplashScreenState extends State<SplashScreen> {
                                 ),
                               ),
                             ],
-                          )
+                          ),
+                          if (i == 2) const SizedBox(height: 5),
+                          if (i == 2)
+                            ElevatedButton(
+                              onPressed: () {
+                                // Handle the "Get Started" button tap
+                              },
+                              child: Text(
+                                'Get Started',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
                         ],
                       ),
                     );
                   },
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
+              // const SizedBox(
+              //   height: 20,
+              // ),
               Padding(
                 padding: const EdgeInsets.all(12.0),
                 child: Row(
@@ -134,12 +326,14 @@ class _SplashScreenState extends State<SplashScreen> {
               ),
             ],
           ),
-          if (isFirstPage)
+          if (currentIndex == 0)
             Positioned(
               top: 0,
               right: 0,
               child: GestureDetector(
-                onTap: () {},
+                onTap: () {
+                  // Handle the "Skip" button tap
+                },
                 child: Container(
                   padding: const EdgeInsets.all(35.0),
                   child: Padding(
