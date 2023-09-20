6381305afbc7db595623e485a5147d190eb27d4a
Muhammad Osama Noor <100303780+Meizzosama@users.noreply.github.com>
2023-09-20 19:20:38 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	lib/help.dart
    #	lib/main.dart
    #	lib/splash_screen/contentpage.dart
    #	lib/splash_screen/sidebar.dart

--- lib/MapPage/mappage.dart
+++ lib/MapPage/mappage.dart
@@ -13,94 +13,92 @@ class _MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 150,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          title: const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                'FIND RESTAURANTS NEAR YOUR',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              Text(
-                'LOCATION',
-                style: TextStyle(fontSize: 30.0),
-              ),
-              Text(
-                'Please allow app access to your location',
-                style: TextStyle(fontSize: 14.0),
-              ),
-            ],
-          ),
-        ),
-        body: Column(
-          children: <Widget>[
-            // Google Map
-            Expanded(
-              child: GoogleMap(
-                initialCameraPosition: const CameraPosition(
-                  target: LatLng(24.8607, 67.0011),
-                  zoom: 12.0,
-                ),
-                markers: <Marker>{
-                  const Marker(
-                    markerId: MarkerId('KarachiMarkerId'),
-                    position: LatLng(24.8607, 67.0011),
-                    infoWindow: InfoWindow(
-                      title: 'Karachi',
-                    ),
-                  ),
-                },
-              ),
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 150,
+        // leading: IconButton(
+        //   icon: const Icon(Icons.arrow_back_ios),
+        //   onPressed: () {
+        //     Navigator.pop(context);
+        //   },
+        // ),
+        title: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 50,
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'homepage');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      fixedSize: const Size(150, 50),
-                    ),
-                    child: const Text(
-                      'Allow',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'mappage');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      fixedSize: const Size(150, 50),
-                    ),
-                    child: const Text(
-                      'Don\'t Allow',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
-              ),
+            Text(
+              'FIND RESTAURANTS NEAR YOUR',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Text(
+              'LOCATION',
+              style: TextStyle(fontSize: 30.0),
+            ),
+            Text(
+              'Please allow app access to your location',
+              style: TextStyle(fontSize: 14.0),
             ),
           ],
         ),
+      ),
+      body: Column(
+        children: <Widget>[
+          // Google Map
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: const CameraPosition(
+                target: LatLng(24.8607, 67.0011),
+                zoom: 12.0,
+              ),
+              markers: <Marker>{
+                const Marker(
+                  markerId: MarkerId('KarachiMarkerId'),
+                  position: LatLng(24.8607, 67.0011),
+                  infoWindow: InfoWindow(
+                    title: 'Karachi',
+                  ),
+                ),
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'homepage');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    fixedSize: const Size(150, 50),
+                  ),
+                  child: const Text(
+                    'Allow',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'mappage');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    fixedSize: const Size(150, 50),
+                  ),
+                  child: const Text(
+                    'Don\'t Allow',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
