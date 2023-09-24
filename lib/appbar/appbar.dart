ffa76797b19922197d3614ae8e498c818d64265e
Muhammad Osama Noor <mosamanoor17@gmail.com>
2023-09-24 15:54:17 +0500

    main

--- lib/appbar/appbar.dart
+++ lib/appbar/appbar.dart
@@ -1,57 +1,7 @@
-// import 'package:flutter/material.dart';
-//
-// class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-//   @override
-//   Size get preferredSize => const Size.fromHeight(kToolbarHeight + 20);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.only(top: 20),
-//       child: AppBar(
-//         leading: IconButton(
-//           icon: const Icon(Icons.menu),
-//           onPressed: () {
-//             Scaffold.of(context).openDrawer();
-//           },
-//           iconSize: 30,
-//         ),
-//         title: Container(
-//           decoration: BoxDecoration(
-//             borderRadius: BorderRadius.circular(30),
-//             color: Colors.grey[300],
-//           ),
-//           child: TextField(
-//             decoration: InputDecoration(
-//               prefixIcon: IconButton(
-//                 icon: const Icon(
-//                   Icons.location_on,
-//                   color: Colors.grey,
-//                 ),
-//                 onPressed: () {},
-//               ),
-//               hintText: "152/2, Main Street, Jersey...",
-//               hintStyle: const TextStyle(fontSize: 14),
-//               border: InputBorder.none,
-//             ),
-//           ),
-//         ),
-//         actions: const [
-//           Padding(
-//             padding: EdgeInsets.only(right: 15.0),
-//             child: Icon(Icons.notification_add),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   final ValueNotifier<int> itemCountNotifier;
-
   MyAppBar({required this.itemCountNotifier});
 
   @override
@@ -109,14 +59,14 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
                     radius: 9,
                     child: Text(
                       itemCountNotifier.value.toString(),
-                      style: TextStyle(
+                      style: const TextStyle(
                         fontSize: 12,
                         color: Colors.white,
                       ),
                     ),
                   ),
                 )
-                    : SizedBox.shrink(),
+                    : const SizedBox.shrink(),
               ],
             ),
           ),
