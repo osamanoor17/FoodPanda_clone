fbee76c56bae09f51450ddbe982b60aa84343338
Muhammad Osama Noor <mosamanoor17@gmail.com>
2023-09-25 21:21:00 +0500

    main

--- lib/invite/invite.dart
+++ lib/invite/invite.dart
@@ -1,66 +1,11 @@
-// import 'package:flutter/material.dart';
-//
-// class InviteFriendScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Invite a Friend'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             const Text(
-//               'Invite your friends to enjoy delicious food with you!',
-//               style: TextStyle(fontSize: 18),
-//               textAlign: TextAlign.center,
-//             ),
-//             const SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: () {},
-//               child: const Row(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   Icon(Icons.message),
-//                   SizedBox(width: 10),
-//                   Text('Send SMS Invitation'),
-//                 ],
-//               ),
-//             ),
-//             ElevatedButton(
-//               onPressed: () {},
-//               child: const Row(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   Icon(Icons.email),
-//                   SizedBox(width: 10),
-//                   Text('Send Email Invitation'),
-//                 ],
-//               ),
-//             ),
-//             ElevatedButton(
-//               onPressed: () {},
-//               child: const Row(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   Icon(Icons.share),
-//                   SizedBox(width: 10),
-//                   Text('Share on Social Media'),
-//                 ],
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 class InviteFriendScreen extends StatelessWidget {
+  void _shareOnSocialMedia(BuildContext context) {
+    String text = 'Check out our awesome app! Download it now: https://play.google.com/store/apps/details?id=com.global.foodpanda.android';
+    Share.share(text, subject: 'App Sharing Subject');
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -101,7 +46,9 @@ class InviteFriendScreen extends StatelessWidget {
               ),
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                _shareOnSocialMedia(context);
+              },
               child: const Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
