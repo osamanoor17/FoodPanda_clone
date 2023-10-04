809c3eb9b97b477dff20ab222a5fd4b12aa70f7e
Muhammad Osama Noor <100303780+Meizzosama@users.noreply.github.com>
2023-10-04 12:21:54 +0500

    main

--- lib/addresses/addresses.dart
+++ lib/addresses/addresses.dart
@@ -31,6 +31,167 @@ class _AddressesState extends State<Addresses> {
     ),
   ];
 
+  TextEditingController nameController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    addressController.dispose();
+    super.dispose();
+  }
+
+  void _editAddress(Address address) {
+    nameController.text = address.name;
+    addressController.text = address.address;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Edit Address'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                ),
+              ),
+              TextFormField(
+                controller: addressController,
+                decoration: const InputDecoration(
+                  labelText: 'Address',
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                _saveEditedAddress(address);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _saveEditedAddress(Address originalAddress) {
+    final newName = nameController.text;
+    final newAddress = addressController.text;
+
+    if (newName.isNotEmpty && newAddress.isNotEmpty) {
+      setState(() {
+        originalAddress.name = newName;
+        originalAddress.address = newAddress;
+      });
+    }
+
+    nameController.clear();
+    addressController.clear();
+  }
+
+  void _navigateToAddAddressScreen() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add Address'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                ),
+              ),
+              TextFormField(
+                controller: addressController,
+                decoration: const InputDecoration(
+                  labelText: 'Address',
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                _saveNewAddress();
+                Navigator.of(context).pop();
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _saveNewAddress() {
+    final newName = nameController.text;
+    final newAddress = addressController.text;
+
+    if (newName.isNotEmpty && newAddress.isNotEmpty) {
+      setState(() {
+        userAddresses.add(Address(name: newName, address: newAddress));
+      });
+    }
+
+    nameController.clear();
+    addressController.clear();
+  }
+
+  void _deleteAddress(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm Deletion'),
+          content: const Text('Are you sure you want to delete this address?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                _confirmDelete(index);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Delete'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _confirmDelete(int index) {
+    setState(() {
+      userAddresses.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,11 +205,22 @@ class _AddressesState extends State<Addresses> {
           return ListTile(
             title: Text(address.name),
             subtitle: Text(address.address),
-            trailing: IconButton(
-              icon: const Icon(Icons.edit),
-              onPressed: () {
-                _editAddress(address);
-              },
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () {
+                    _editAddress(address);
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    _deleteAddress(index);
+                  },
+                ),
+              ],
             ),
           );
         },
@@ -61,14 +233,11 @@ class _AddressesState extends State<Addresses> {
       ),
     );
   }
-
-  void _editAddress(Address address) {}
-  void _navigateToAddAddressScreen() {}
 }
 
 class Address {
-  final String name;
-  final String address;
+  String name;
+  String address;
 
   Address({required this.name, required this.address});
 }
